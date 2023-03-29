cc89f9ec3ffbd978e3b038059d80b96f32f730c1
damian <qaqa.tanski@users.noreply.github.com>
2023-03-29 19:23:28 +0200

    Initial commit

---
+++ src/main/java/sia/tacocloudOAuth2server/TacoCloudOAuth2ServerApplication.java
@@ -0,0 +1,13 @@
+package sia.tacocloudOAuth2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudOAuth2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudOAuth2ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sia/tacocloudOAuth2server/TacoCloudOAuth2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.tacocloudOAuth2server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudOAuth2ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
